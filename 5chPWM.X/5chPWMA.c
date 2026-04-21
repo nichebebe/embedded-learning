957f98f4e5266fb27048e1199812c725d27b10e3
nichebebe <moon.and.kimitati@gmail.com>
2026-04-21 22:52:08 +0900

    Add Timer and ADC init settings

--- 5chPWM.X/5chPWMA.c
+++ 5chPWM.X/5chPWMA.c
@@ -24,4 +24,28 @@ void PWM_Init(void){
     APFCONbits.CCP2SEL = 0;
     APFCONbits.CCP3SEL = 1;
     
+    T2CONbits.T2CKPS = 0b10;
+    T4CONbits.T4CKPS = 0b10;
+    T6CONbits.T6CKPS = 0b10;
+    
+    T2CONbits.TMR2ON = 1;
+    T4CONbits.TMR4ON = 1;
+    T6CONbits.TMR6ON = 1;
+    
+    PR2 = 124;
+    PR4 = 124;
+    PR6 = 124;
+    
+    CCP1CON = 0b00001100;
+    CCP2CON = 0b00001100;
+    CCP3CON = 0b00001100;
+    CCP4CON = 0b00001100;
+    CCP5CON = 0b00001100;
+}
+
+void ADC_Init(void)
+{
+    TRISA = 0b00101111;
+    ANSELA = 0b00101111;
+    ADCON0bits.ADON = 1;
 }
