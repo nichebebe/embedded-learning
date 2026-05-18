876022b7445c8e6a9956b25e537519ca4232d990
nichebebe <moon.and.kimitati@gmail.com>
2026-05-18 15:35:02 +0900

    Implement basic I2C LCD control

--- LCD_Practice.X/LCD_setting.c
+++ LCD_Practice.X/LCD_setting.c
@@ -20,6 +20,20 @@
 
 unsigned char str[]= "Hello";
 
+void I2C_Init(void)
+{
+    TRISCbits.TRISC3 = 1;
+    TRISCbits.TRISC4 = 1;
+    
+    SSPSTATbits.SMP = 1;   //I2C nomal mode
+    SSPSTATbits.CKE = 0;   //SMBus input disabled
+    
+    SSPCON1bits.SSPM = 1000;
+    SSPADD = 19;
+    
+    SSPCON1bits.SSPEN = 1;
+}
+
 void I2C_Start(void)
 {
     SSPCON2bits.SEN = 1;
@@ -29,7 +43,6 @@ void I2C_Start(void)
 void I2C_Write(unsigned char data)
 {
     PIR1bits.SSPIF = 0;
-    data = data;
     SSPBUF = data;
     while(!PIR1bits.SSPIF);
 }
@@ -95,12 +108,17 @@ void LCD_Init(void) {
 
 void main(void)
 {
+    I2C_Init();
     LCD_Init();
-    for(unsigned char i = 0; i < 16; i++)
+    
+    for(unsigned char i = 0; str[i] != '\0'; i++)
     {
         write_Data(str[i]);
     }
     write_Command(0xC0); //2?????
     
-    
+    while(1)
+    {
+        
+    }
 }
